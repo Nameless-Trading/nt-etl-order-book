ea593ea7eff6358e81476cba7b48c8b5905bc375
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-11-11 08:54:36 -0700

    Finished main function.

--- nt_etl_order_book/__main__.py
+++ nt_etl_order_book/__main__.py
@@ -1,11 +1,18 @@
 import asyncio
 
 from consumer import Consumer
+from producer import Producer
+
+async def main():
+    series_ticker = "KXNCAAFGAME"
+    producer = Producer()
+    consumer = Consumer()
+
+    # Run producer and consumer concurrently
+    await asyncio.gather(
+        producer.run(series_ticker=series_ticker),
+        consumer.run()
+    )
 
 if __name__ == "__main__":
-    # series_ticker = "KXNCAAFGAME"
-    # producer = Producer()
-    # asyncio.run(producer.run(series_ticker=series_ticker))
-
-    consumer = Consumer()
-    asyncio.run(consumer.run())
+    asyncio.run(main())
